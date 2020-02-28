897bca65e4e05678d0cd52c53c7f843103d07dc4
xiao.han <xiao.han@dbappsecurity.com.cn>
2020-02-28 08:30:24 +0800

    更新

--- updateInfo.sh
+++ updateInfo.sh
@@ -1,4 +1,6 @@
-dpkg-scanpackages deb/*.deb /dev/null > Packages
+dpkg-scanpackages deb/ > Packages
 sed -i "" 's/deb\//\.\/deb\//g' Packages
+rm -f Packages.gz
+rm -f Packages.bz2
 tar zcvf Packages.gz Packages
 bzip2 -k Packages Packages.bz2
