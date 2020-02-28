d4cc8bf84af30867b35cd517fae2784998c12a70
xiao.han <xiao.han@dbappsecurity.com.cn>
2020-02-28 08:41:55 +0800

    更新

--- updateInfo.sh
+++ updateInfo.sh
@@ -1,6 +1,9 @@
-dpkg-scanpackages deb/ > Packages
+sudo dpkg-scanpackages deb/ > Packages
 sed -i "" 's/deb\//\.\/deb\//g' Packages
 rm -f Packages.gz
 rm -f Packages.bz2
 tar zcvf Packages.gz Packages
 bzip2 -k Packages Packages.bz2
+git add .
+git commit -m '更新'
+git push
